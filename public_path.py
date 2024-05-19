f66d4cadfc15e88e272b3db46e172b285ffd881f
1029920178@qq.com <1029910278@qq.com>
2024-05-20 06:14:50 +0800

    add true path

--- public_path.py
+++ public_path.py
@@ -1,5 +1,6 @@
 import os
 import json
+import sys
 from pathlib import Path
 from typing import Union
 
@@ -8,4 +9,16 @@ def get_b3d_ext_dir(version: str = '4.2') -> Path:
     return Path.home().joinpath('AppData', 'Roaming', 'Blender Foundation', 'Blender', version, 'extensions')
 
 
+def true_path() -> Path:
+    if hasattr(sys, '_MEIPASS'):
+        return Path(sys._MEIPASS)
+    return Path(__file__).parent
+
+
+def get_bme_db() -> Path:
+    return true_path().joinpath('bme_db')
+
+
+def get_config():
+    return get_bme_db().joinpath('config.json')
 
