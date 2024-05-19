40a2735f50cc9a9ff3b51dce80b3de13d6845709
1029920178@qq.com <1029910278@qq.com>
2024-05-19 19:23:15 +0800

    add keyboard handle to dialog

--- view_model/widget_ext_card_edit_dialog.py
+++ view_model/widget_ext_card_edit_dialog.py
@@ -1,5 +1,5 @@
-from nicegui import ui
-from model.schema import Schema
+from nicegui import ui, events
+from model.schema import Schema, ExtensionsOptional
 from translation import _p
 
 
@@ -7,6 +7,8 @@ class CardEditDialog(ui.dialog):
     def __init__(self, data: dict):
         super().__init__()
         self.scheme = Schema(data)
+        self.keyboard = ui.keyboard(on_key=self.handle_keyboard)
+
         with self.props('position="right"'), ui.card().classes('w-full items-center').props('rounded'):
             with ui.element('q-list').props('bordered'):
                 ui.input(label=_p('name')).bind_value(self.scheme, 'name').style('min-width:400px; max-width: 600px')
@@ -19,7 +21,19 @@ class CardEditDialog(ui.dialog):
                         continue
                     else:
                         ui.input(label=_p(k)).bind_value(self.scheme, k).style('min-width:400px; max-width: 600px')
+                ui.separator()
+                with ui.element('q-list').props('bordered'):
+                    for k in ExtensionsOptional.__annotations__.keys():
+                        if hasattr(self.scheme, k):
+                            if k in ['permissions', 'platforms']: continue
+                            ui.input(label=_p(k)).bind_value(self.scheme, k).style('min-width:400px; max-width: 600px')
 
             with ui.row():
                 ui.button(_p('Cancel'), on_click=lambda: self.submit(None)).props('flat')
                 ui.button(_p('Save'), on_click=lambda: self.submit(self.scheme.to_dict()))
+
+    def handle_keyboard(self, e: events.KeyEventArguments):
+        if e.key == 'Enter':
+            self.submit(self.scheme.to_dict())
+        elif e.key == 'Escape':
+            self.submit(None)
