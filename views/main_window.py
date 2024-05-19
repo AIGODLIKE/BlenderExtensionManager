593e7147436e432f592e0ed923f8157b793cfa89
1029920178@qq.com <1029910278@qq.com>
2024-05-19 19:01:43 +0800

    add tab, change tab style

--- views/main_window.py
+++ views/main_window.py
@@ -8,17 +8,18 @@ def draw():
         ui.button(on_click=app.shutdown, icon='close').props('flat color=white')
 
     with ui.left_drawer().classes('w-full px-0 p-0').props('width=100 breakpoint=500') as left_drawer:
-        with ui.tabs().props(
+        with ui.tabs().classes('text-grey-6').props(
                 'switch-indicator vertical swipeable active-color="primary" indicator-color="primary"') as tabs:
-            # ui.image(get_image('blender.png')).props('w-full h-1/2')
             with ui.tab('Extensions', icon='list').props('flat color=white'):
                 # ui.badge('5+').props('color="red" floating')
                 pass
+            ui.tab('Convert', icon='change_circle').props('flat color=white')
             ui.tab('Settings', icon='settings').props('flat color=white')
-
     with ui.tab_panels(tabs, value='Extensions').classes('w-full h-full px-0 p-0').props(
             'transition-prev=jump-up transition-next=jump-up'):
         with ui.tab_panel('Extensions'):
             extension_view.draw()
+        with ui.tab_panel('Convert'):
+            pass
         with ui.tab_panel('Settings'):
             pass
