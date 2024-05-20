f98e5a8673a3ea585ca880b863617aa4a734ee04
1029920178@qq.com <1029910278@qq.com>
2024-05-20 19:50:26 +0800

    add build

---
+++ build.py
@@ -0,0 +1,40 @@
+import subprocess
+from pathlib import Path
+
+APP_NAME = 'BME'
+icon_path = ''
+
+
+def build_windows():
+    return [
+        'python',
+        '-m', 'PyInstaller',
+        'main.py',  # your main file with ui.run()
+        '--name', APP_NAME,  # name of your app
+        '--onefile',  # one file exe --ondir
+        '--windowed',
+        '-y',  # overwrite output folder
+        # '--icon', f'{res_path().joinpath("res", "images", "icon.ico").resolve()}'
+    ]
+
+
+def common_data_params() -> list:
+    import os
+    import nicegui
+    parms = [
+        '--noconsole',  # 不显示控制台
+        '--add-data', f'{Path(nicegui.__file__).parent}{os.pathsep}nicegui',
+        '--add-data', f'bme_db{os.pathsep}bme_db'  # you can add your own additional resources
+    ]
+
+    return parms
+
+
+if __name__ == '__main__':
+    print(build_windows())
+    print(common_data_params())
+    print(APP_NAME)
+    print(icon_path)
+
+    p = subprocess.Popen(build_windows() + common_data_params())
+    p.communicate()
--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import sys
+import nicegui
 from nicegui import ui, app
 from views import main_window
 
+DEV_MODE = True
+
 if __name__ in {"__main__", "__mp_main__"}:
     main_window.draw()
-    if len(sys.argv) > 1 and sys.argv[1] == "--web":
-        ui.run()
+    args = {
+        'title': "Blender Extension Manager",
+        'reload': DEV_MODE,  # true for develop
+    }
+
+    if len(sys.argv) > 1 and "--web" in sys.argv:
+        _args = {}
     else:
-        ui.run(native=True, window_size=(1280, 720),frameless=True, title="Blender Extension Manager")
+        _args = {
+            'native': True,
+            'window_size': (1280, 720),
+            'frameless': True,
+        }
+    args.update(_args)
+    ui.run(**args)
