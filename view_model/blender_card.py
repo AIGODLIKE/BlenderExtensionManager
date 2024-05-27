6727ca9e5e2dbbf5f89c2884cde30e4c291b50e0
1029920178@qq.com <1029910278@qq.com>
2024-05-28 06:23:26 +0800

    fix ui update

--- view_model/blender_card.py
+++ view_model/blender_card.py
@@ -7,6 +7,7 @@ import os
 from typing import Union, Optional
 from model.blender import Blender
 from public_path import get_icon_path
+from view_model.global_worker import Worker
 
 
 def open_file(file: Path):
@@ -36,15 +37,14 @@ async def select_blender(container: ui.row):
 
 
 async def verify_blender(b3d: Blender, set_active=True) -> Union[Blender, bool]:
-    n = ui.notification(message=_p("Verify Blender..."), spinner=True, type="ongoing", timeout=None)
+    n = ui.notification(message=_p("Verify Blender..."), spinner=True, type="ongoing", timeout=3)
 
     async def _error_n():
         n.message = _p('Invalid Blender')
         n.spinner = False
         n.icon = 'error'
         n.type = 'negative'
-        await asyncio.sleep(1)
-        n.dismiss()
+        n.close_button = _p('OK')
 
     await asyncio.sleep(0.5)
     if not Path(b3d.path).exists():
@@ -77,8 +77,6 @@ async def verify_blender(b3d: Blender, set_active=True) -> Union[Blender, bool]:
     if set_active:
         b3d.is_active = True
 
-    await asyncio.sleep(1)
-    n.dismiss()
     return b3d
 
 
@@ -90,40 +88,31 @@ class BlenderCard(ui.card):
         self.classes('w-64 h-48 no-shadow').props('bordered')
 
         with self:
-            with ui.dialog() as dialog, ui.card().classes('w-96 items-center'):
-                path_input = ui.input(value=b3d.path).classes('w-full').props(
-                    'dense flat color="primary" outlined autogrow').bind_value(b3d, 'path')
-                with path_input.add_slot('append'):
-                    ui.button(icon='folder').props('dense flat color="primary"')
-
-                with ui.row().classes('w-full items-center'):
-                    ui.button(_p('Cancel'), on_click=dialog.close).props('dense flat color="primary"')
-                    ui.button(_p('Save'), on_click=lambda: dialog.submit(b3d.save_to_db()))
-
             with ui.column().classes('w-full items-start gap-1'):
                 ui.image(get_icon_path('blender.png')).classes('h-1/3') \
                     .style('filter: grayscale(100%)').bind_visibility_from(self.blender, 'is_active', lambda v: not v)
                 ui.image(get_icon_path('blender.png')).classes('h-1/3').bind_visibility_from(self.blender, 'is_active')
 
-                with ui.row().classes('w-full items-center px-0 gap-1'):
-                    ui.label(b3d.version).classes('text-xl')
-
-                    ui.space()
-                    with ui.button(icon='info', on_click=lambda: open_file(Path(b3d.path))) \
-                            .props('dense flat color="primary rounded"'):
-                        with ui.tooltip().classes(f'text-lg bg-primary shadow-2'):
-                            with ui.element('q-list'):
-                                ui.label(f'Version: {b3d.version}')
-                                ui.label(f'Date: {b3d.date}')
-                                ui.label(f'Hash: {b3d.hash}')
-                                ui.label(f'Path: {b3d.path}')
-                    ui.button(icon='edit', on_click=lambda: dialog).props('dense rounded flat color="primary"')
-                    ui.button(icon='close', on_click=self.remove_blender).props(
-                        'dense rounded flat color="red"')
-                with ui.column() as self.active_draw:
+                with ui.column().classes('w-full items-start gap-1') as self.active_draw:
                     self.draw_active()
 
     def draw_active(self):
+        b3d = self.blender
+        with ui.row().classes('w-full items-center px-0 gap-1'):
+            ui.label(b3d.version).classes('text-xl')
+
+            ui.space()
+            with ui.button(icon='info', on_click=lambda: open_file(Path(b3d.path))) \
+                    .props('dense flat color="primary rounded"'):
+                with ui.tooltip().classes(f'text-lg bg-primary shadow-2'):
+                    with ui.element('q-list'):
+                        ui.label(f'Version: {b3d.version}')
+                        ui.label(f'Date: {b3d.date}')
+                        ui.label(f'Hash: {b3d.hash}')
+                        ui.label(f'Path: {b3d.path}')
+            ui.button(icon='close', on_click=self.remove_blender).props(
+                'dense rounded flat color="red"')
+
         ui.button(_p('Invalid')).classes('text-red-5 text-lg') \
             .bind_visibility_from(self.blender, 'is_valid', lambda v: not v) \
             .props('no-caps flat dense')
@@ -145,7 +134,7 @@ class BlenderCard(ui.card):
                 c.blender.save_to_db()
 
         await verify_blender(self.blender, set_active=False)
-        for c in self.container:
+        for c in self.container.default_slot.children:
             if isinstance(c, BlenderCard):
                 c.active_draw.clear()
                 with c.active_draw:
@@ -154,9 +143,7 @@ class BlenderCard(ui.card):
     def remove_blender(self):
         self.blender.remove_from_db()
         ui.notify(_p('Removed'))
-        self.container.clear()
-        with self.container:
-            load_all(self.container)
+        self.delete()
 
 
 def load_all(container: ui.row):
